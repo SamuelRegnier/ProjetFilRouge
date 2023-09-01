f78a6c1f4b74234349e18f7bce9ec0847cd97528
Samir2113 <Samir2113.samir-sofiane@hotmail.fr>
2023-09-01 15:51:45 +0200

    Modif de BooleanValidation.java

--- src/main/java/com/filrouge/ProjetFilRouge/validation/BooleanValidation.java
+++ src/main/java/com/filrouge/ProjetFilRouge/validation/BooleanValidation.java
@@ -1,5 +1,19 @@
 package com.filrouge.ProjetFilRouge.validation;
 
-public class BooleanValidation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BooleanValidation {
+	
+public static List <String> ErreurBoolean (Boolean bool) {
+		
+		List <String> listErreurs= new ArrayList <> ();
+		
+		if (bool != true || bool != false) {
+			listErreurs.add("L'entrée doit être un boolean; ");
+			return listErreurs;
+		}
+		
+		return listErreurs;
+	}
 }
